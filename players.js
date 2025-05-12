c9151e149dac7ecbcce726bd898bd660fa5cdcc3
Shagan742 <526ssangha@frhsd.com>
2025-05-12 10:40:27 -0400

    starting pt

---
+++ players.js
@@ -0,0 +1,41 @@
+const players= [
+    {
+        firstName:'Jonas',
+        lastName:'Paul',
+        position:'SS',
+        age:52,
+        photo:'https://picsum.photos/200/300'
+    },
+
+    {
+        firstName:'Jonas',
+        lastName:'Paul',
+        position:'SS',
+        age:52,
+        photo:'https://picsum.photos/200/300'
+    },
+
+    {
+        firstName:'Jonas',
+        lastName:'Paul',
+        position:'SS',
+        age:52,
+        photo:'https://picsum.photos/200/300'
+    },
+
+    {
+        firstName:'Jonas',
+        lastName:'Paul',
+        position:'SS',
+        age:52,
+        photo:'https://picsum.photos/200/300'
+    },
+
+    {
+        firstName:'Jonas',
+        lastName:'Paul',
+        position:'SS',
+        age:52,
+        photo:'https://picsum.photos/200/300'
+    }
+]
---
+++ roster.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+    const grid=document.getElementById('rosterGrid')
+    const render= list => {
+        grid.innerHTML=''
+        list.forEach(player => {
+            const col = document.createElement('div')
+            col.className='col-6 col-lg-2'
+            col.innerHTML=`
+            
+            <div class="card h-100 shadow-sm">
+                    <img src="${player.photo}" alt="${player.firstName} ${player.lastName}" class="card-img-top">
+                    <div class="card-body text-center">
+                        <h5 class="card-title mb-1">${player.firstName} ${player.lastName}</h5>
+                        <div class="badge badge-position badge-pos-${player.position}">${player.position}</div>
+                        <p class="small text-muted mb-0">Age: ${player.age}</p>
+                    </div>
+                </div>
+            
+            `
+            grid.appendChild(col)
+        })
+
+
+    }
+
+
+
+
+
+
+render(players)
+})
