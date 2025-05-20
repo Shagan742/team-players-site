b31b03d679ddd814e31f499bb87c9419bd3bd6ee
Shagan742 <526ssangha@frhsd.com>
2025-05-20 17:14:48 +0000

    i dont know why it wont work

--- roster.js
+++ roster.js
@@ -13,9 +13,19 @@ document.addEventListener('DOMContentLoaded', ()=> {
                         <h5 class="card-title mb-1">${player.firstName} ${player.lastName}</h5>
                         <div class="badge badge-position badge-pos-${player.position}">${player.position}</div>
                         <p class="small text-muted mb-0">Age: ${player.age}</p>
-                        <btn class="btn bg-secondary onclick="this.style.display.block"">More Info</btn>
-                        <div id="modal" onclick="this.style.display.none"></div>
+                    
+                    <btn class="btn bg-secondary onclick="document.getElementById('modal`+ increaseNumber() +`').style.display='block' "">More Info</btn>
+                        <div id="modal` + increaseNumber() + `">
+                            <div class="modal-content">
+                                <div class="modal-container">
+                                <span onclick="document.getElementById('modal` + increaseNumber() + `').style.display='none'">&times</span>
+                                <p>shskjfdhsdkldfhasklfhsdlhfkls</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
+
+
                 </div>
             
             `
@@ -25,11 +35,16 @@ document.addEventListener('DOMContentLoaded', ()=> {
             
         })
 
+        
+
 
     }
 
 
-
+function increaseNumber() {
+            let x = 0;
+            x++;
+        }
 
 render(players)
 })
