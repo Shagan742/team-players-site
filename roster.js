5de2f540095c4d230dba85749b7f641a12155b5b
Shagan742 <526ssangha@frhsd.com>
2025-05-21 09:52:37 -0400

    a

--- roster.js
+++ roster.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', ()=> {
                         <h5 class="card-title mb-1">${player.firstName} ${player.lastName}</h5>
                         <div class="badge badge-position badge-pos-${player.position}">${player.position}</div>
                         <p class="small text-muted mb-0">Age: ${player.age}</p>
-                        <btn class="btn bg-secondary onclick="this.style.display.block"">More Info</btn>
+                        <btn class="btn bg-secondary" onclick="this.style.display.block">More Info</btn>
                         <div id="modal" onclick="this.style.display.none"></div>
                     </div>
                 </div>
