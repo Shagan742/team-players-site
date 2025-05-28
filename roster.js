1769c9e6a21221a6e68aed1d469c5be55f41b2b9
Shagan742 <526ssangha@frhsd.com>
2025-05-28 11:30:51 -0400

    modal works but at what cost, player i thing doesnt increase

--- roster.js
+++ roster.js
@@ -2,10 +2,10 @@ document.addEventListener('DOMContentLoaded', ()=> {
     const grid=document.getElementById('rosterGrid')
     const render= list => {
         grid.innerHTML=''
-        list.forEach(player, i => {
+        list.forEach(player => {
             const col = document.createElement('div')
             col.className='col-6 col-lg-2'
-            let i = 0;
+            let i=0;
             i++;
             col.innerHTML=`
             
@@ -15,25 +15,31 @@ document.addEventListener('DOMContentLoaded', ()=> {
                         <h5 class="card-title mb-1">${player.firstName} ${player.lastName}</h5>
                         <div class="badge badge-position badge-pos-${player.position}">${player.position}</div>
                         <p class="small text-muted mb-0">Age: ${player.age}</p>
-                        <btn class="btn bg-secondary" onclick="document.getElementById('modal${i}').style.display='block'">More Info</btn>
 
-<div id="modal${i}" class="modal${i}" tabindex="-1">
-  <div class="modal${i}-dialog">
-    <div class="modal${i}-content">
-      <div class="modal${i}-header">
-        <h5 class="modal${i}-title">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal${i}" aria-label="Close"></button>
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal${i}">
+  More Info
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal${i}" tabindex="-1" aria-labelledby="exampleModalLabel${i}" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel${i}">Fun Fact ${i}:</h1>
       </div>
-      <div class="modal${i}-body">
-        <p>hgjgjgh</p>
+      <div class="modal-body">
+        ${player.hiddenFact}
       </div>
-      <div class="modal${i}-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal${i}">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
       </div>
     </div>
   </div>
 </div>
+
+
+
                     </div>
                 </div>
 
@@ -43,6 +49,29 @@ document.addEventListener('DOMContentLoaded', ()=> {
                    
             
             `
+
+
+
+// <btn class="btn bg-secondary" onclick="document.getElementById('modal${i}').style.display='block'">More Info</btn>
+
+// <div id="modal${i}" class="modal" tabindex="-1">
+//   <div class="modal-dialog">
+//     <div class="modal-content">
+//       <div class="modal-body" id="modal${i}-body">
+//         ${player.hiddenFact}
+//       </div>
+//       <div class="modal-footer">
+//         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+//       </div>
+//     </div>
+//   </div>
+// </div>
+
+
+
+
+
+
     //  <div id="modal${i}" class="modal${i}" onclick="document.getElementById('modal${i}').style.display='none'">
     //                     <div class="modal${i}-content">
     //                     <div class="modal${i}-container">
