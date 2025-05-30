eb2c5b70151dec73aec25de28a4f9311445bd295
Shagan742 <526ssangha@frhsd.com>
2025-05-30 09:31:51 -0400

    put data in

--- players.js
+++ players.js
@@ -1,46 +1,82 @@
 const players= [
     {
-        firstName:'Jonas',
-        lastName:'Paul',
-        position:'SS',
-        age:52,
-        photo:'https://picsum.photos/200/300',
-        hiddenFact:'1'
+        firstName:'Eduardo',
+        lastName:'Sánchez',
+        position:'Co-director & writer',
+        age:56,
+        photo:'imgs/1.jpg',
+        hiddenFact:'Eduardo was born in Cuba, 6 years after the Cuban Missile Crisis'
     },
 
     {
-        firstName:'Jonas',
-        lastName:'Paul',
-        position:'SS',
-        age:52,
-        photo:'https://picsum.photos/200/300',
-        hiddenFact:'2'
+        firstName:'Daniel',
+        lastName:'Myrick',
+        position:'Co-director & writer',
+        age:61,
+        photo:'imgs/2.jpg',
+        hiddenFact:'He worked with actor Michael C. Williams in his projects many times. '
     },
 
     {
-        firstName:'Jonas',
-        lastName:'Paul',
-        position:'SS',
-        age:52,
-        photo:'https://picsum.photos/200/300',
-        hiddenFact:'3'
+        firstName:'Robin',
+        lastName:'Cowie',
+        position:'Film producer',
+        age:55,
+        photo:'imgs/3.webp',
+        hiddenFact:'Robin Cowie founded Skillmaker.AI, an AI startup.'
     },
 
     {
-        firstName:'Jonas',
-        lastName:'Paul',
-        position:'SS',
-        age:52,
-        photo:'https://picsum.photos/200/300',
-        hiddenFact:'4'
+        firstName:'Ben',
+        lastName:'Rock',
+        position:'Production Designer',
+        age:54,
+        photo:'imgs/4.jpg',
+        hiddenFact:'He is the son of former Orlando and Miami area Bozo the Clown Alan Rock.'
     },
 
     {
-        firstName:'Jonas',
-        lastName:'Paul',
-        position:'SS',
-        age:52,
-        photo:'https://picsum.photos/200/300',
-        hiddenFact:'5'
+        firstName:'Gregg',
+        lastName:'Hale',
+        position:'Film producer',
+        age:59,
+        photo:'imgs/5.webp',
+        hiddenFact:'Gregg used a Super 8 camera to make short films, stop-motion animation and zombie movies, conveying interest in film at a young age.'
+    },
+
+    {
+        firstName:'Bob',
+        lastName:'Heick',
+        position:'Executive Producer',
+        age:51,
+        photo:'imgs/6.jpg',
+        hiddenFact:'Bob is a Connecticut kind of guy, specifically preferring Bridgeport.'
+    },
+
+    {
+        firstName:'Neal',
+        lastName:'Fredericks',
+        position:'Cinematographer',
+        age:56,
+        photo:'imgs/7.webp',
+        hiddenFact:'Unfortunately, Neal is not alive anymore because of having suffered in a fatal plane crash since 2004. RIP'
+    },
+
+    {
+        firstName:'Kevin',
+        lastName:'Foxe',
+        position:'Writer & Storyteller',
+        age:61,
+        photo:'imgs/8.webp',
+        hiddenFact:'8'
+    },
+
+    {
+        firstName:'Tony',
+        lastName:'Cora',
+        position:'Music composer',
+        age:'??',
+        photo:'imgs/9.webp',
+        hiddenFact:'Tony\'s date of birth is not known, hence the ?? in the age category.'
     }
 ]
--- roster.js
+++ roster.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', ()=> {
             col.className='col-6 col-lg-2'
             col.innerHTML=`
             
-            <div class="card h-100 shadow-sm">
+            <div class="card h-100 shadow-sm" style="width:210px;">
                     <img src="${player.photo}" alt="${player.firstName} ${player.lastName}" class="card-img-top">
                     <div class="card-body text-center">
                         <h5 class="card-title mb-1">${player.firstName} ${player.lastName}</h5>
@@ -23,6 +23,11 @@ document.addEventListener('DOMContentLoaded', ()=> {
 <div class="modal fade" id="exampleModal${i}" tabindex="-1" aria-labelledby="exampleModalLabel${i}" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
+
+    <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel${i}">${player.firstName} ${player.lastName}</h1>
+      </div>
+
       <div class="modal-body">
         <p class="hiddenFact">${player.hiddenFact}</p>   
        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
