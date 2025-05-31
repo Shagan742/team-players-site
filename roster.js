f79fc282d74bb0afa6b7a81a61760d3d94a6e046
Shagan742 <526ssangha@frhsd.com>
2025-05-31 16:51:49 +0000

    now i just need fonts

--- roster.js
+++ roster.js
@@ -4,9 +4,8 @@ document.addEventListener('DOMContentLoaded', ()=> {
         grid.innerHTML=''
         list.forEach((player, i) => {
             const col = document.createElement('div')
-            col.className='col-6 col-lg-2'
+            col.className='col-md-4 col-sm-12 d-flex justify-content-center my-5'
             col.innerHTML=`
-            
             <div class="card h-100 shadow-sm" style="width:210px;">
                     <img src="${player.photo}" alt="${player.firstName} ${player.lastName}" class="card-img-top">
                     <div class="card-body text-center">
@@ -21,8 +20,8 @@ document.addEventListener('DOMContentLoaded', ()=> {
 
 <!-- Modal -->
 <div class="modal fade" id="exampleModal${i}" tabindex="-1" aria-labelledby="exampleModalLabel${i}" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
+  <div class="modal-dialog" >
+    <div class="modal-content" style="background-color:black; border:2px solid white;">
 
     <div class="modal-header">
         <h1 class="modal-title fs-5" id="exampleModalLabel${i}">${player.firstName} ${player.lastName}</h1>
@@ -40,43 +39,7 @@ document.addEventListener('DOMContentLoaded', ()=> {
 
                     </div>
                 </div>
-
-
-
-                        
-                   
-            
             `
-
-
-
-// <btn class="btn bg-secondary" onclick="document.getElementById('modal${i}').style.display='block'">More Info</btn>
-
-// <div id="modal${i}" class="modal" tabindex="-1">
-//   <div class="modal-dialog">
-//     <div class="modal-content">
-//       <div class="modal-body" id="modal${i}-body">
-//         ${player.hiddenFact}
-//       </div>
-//       <div class="modal-footer">
-//         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-//       </div>
-//     </div>
-//   </div>
-// </div>
-
-
-
-
-
-
-    //  <div id="modal${i}" class="modal${i}" onclick="document.getElementById('modal${i}').style.display='none'">
-    //                     <div class="modal${i}-content">
-    //                     <div class="modal${i}-container">
-    //                     <p class="paragraph">Fun Fact: ${this.hiddenFact}</p>
-    //                     </div>
-    //                     </div>
-    //                     </div>
 
             grid.appendChild(col)
 
